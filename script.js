d71226d75f8c4c3a8e1831f3899d3662f5f84493
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-01-15 23:50:47 -0700

    finished save button function

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 let date = moment().format("MMMM / DD / YYYY")
 console.log(date)
+
+// function hoursUpdater() {
+//     var currentHour = moment().hours();
+//     console.log(currentHour);
+
+//     $("timeBlock").each(function () {
+
+//         var blockHour = parseInt($(this).attr("id"))
+//     }
+// }}
+
+$(".saveButton").on("click", function (e) {
+
+
+    let myValue = $(this).siblings('section').children('input').val()
+    let myKey = $(this).siblings('span').attr('id')
+
+    localStorage.setItem(myKey, myValue)
+})
