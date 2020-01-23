693ce9708fbd5d5626cdbf35a6eac2c29a2902d1
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-01-23 16:38:59 -0700

    updated JS to get my functions to work but still isnt and changed my timeblock class name

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-$("#currentDay").text(moment());
-setInterval(function () {
-    $("#currentDay").text(moment());
-}, 1000);
+// $("#currentDay").text(moment());
+// setInterval(function () {
+//     $("#currentDay").text(moment());
+// }, 1000);
 
 var date = moment().format("MMMM / DD / YYYY")
-console.log(date)
 
+console.log(date)
+//This function executes the hour and color coordinator referencing what time it is
 function hoursUpdater() {
     var currentHour = moment().hours();
     console.log(currentHour);
 
-    $(".time-Block").each(function () {
+    $(".timeBlock").each(function () {
         var blockValue = $(this).attr("id")
         var blockHour = parseInt($(this).attr('id').split("-")[1])
         var blockResult = localStorage.getItem(blockValue)
         $(this).children('textarea').text(blockResult)
-
+        console.log(blockValue)
         if (blockHour < currentHour) {
             $(this).children("textarea").addClass("past")
-            console.log("Block Hour is before current hour");
         } else if (blockHour === currentHour) {
             $(this).children("textarea").addClass("present")
         } else {
@@ -26,7 +26,7 @@ function hoursUpdater() {
         }
     })
 }
-
+//This is my JQuery onclick function for my button that saves it to local storage
 $(".saveBtn").on("click", function () {
 
     var myValue = $(this).parent().attr('id').val();
@@ -34,4 +34,4 @@ $(".saveBtn").on("click", function () {
 
     localStorage.setItem(myKey, myValue)
 })
-hoursUpdater()
+hoursUpdater();
